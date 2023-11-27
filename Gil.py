717ba571ac2f344ab95a3dfe1ff6c88af1abfbf4
NoyaCh <80586356+NoyaCh@users.noreply.github.com>
2023-11-27 18:59:50 +0200

    Update Gil.py

--- Gil.py
+++ Gil.py
@@ -1,2 +1,3 @@
 print("Hi gil")
-print("now I am trying to see if I edit here, I can see a change in the local)
+print("now I am trying to see if I edit here, I can see a change in the local")
+print("3)      
